979bfaac612fc1927cc6d51c77ffc269a8ed801c
Josh Schonstal <josh@schonstal.org>
2014-08-22 18:01:59 -0700

    Boilerplate

---
+++ export/mac64/cpp/obj/src/MenuState.cpp
@@ -0,0 +1,152 @@
+#include <hxcpp.h>
+
+#ifndef INCLUDED_MenuState
+#include <MenuState.h>
+#endif
+#ifndef INCLUDED_flixel_FlxBasic
+#include <flixel/FlxBasic.h>
+#endif
+#ifndef INCLUDED_flixel_FlxState
+#include <flixel/FlxState.h>
+#endif
+#ifndef INCLUDED_flixel_group_FlxGroup
+#include <flixel/group/FlxGroup.h>
+#endif
+#ifndef INCLUDED_flixel_group_FlxTypedGroup
+#include <flixel/group/FlxTypedGroup.h>
+#endif
+#ifndef INCLUDED_flixel_interfaces_IFlxDestroyable
+#include <flixel/interfaces/IFlxDestroyable.h>
+#endif
+
+Void MenuState_obj::__construct(Dynamic MaxSize)
+{
+HX_STACK_FRAME("MenuState","new",0xe563b1c4,"MenuState.new","MenuState.hx",13,0xdfbcb22c)
+HX_STACK_THIS(this)
+HX_STACK_ARG(MaxSize,"MaxSize")
+{
+	HX_STACK_LINE(13)
+	super::__construct(MaxSize);
+}
+;
+	return null();
+}
+
+//MenuState_obj::~MenuState_obj() { }
+
+Dynamic MenuState_obj::__CreateEmpty() { return  new MenuState_obj; }
+hx::ObjectPtr< MenuState_obj > MenuState_obj::__new(Dynamic MaxSize)
+{  hx::ObjectPtr< MenuState_obj > result = new MenuState_obj();
+	result->__construct(MaxSize);
+	return result;}
+
+Dynamic MenuState_obj::__Create(hx::DynamicArray inArgs)
+{  hx::ObjectPtr< MenuState_obj > result = new MenuState_obj();
+	result->__construct(inArgs[0]);
+	return result;}
+
+Void MenuState_obj::create( ){
+{
+		HX_STACK_FRAME("MenuState","create",0xe57b7c18,"MenuState.create","MenuState.hx",20,0xdfbcb22c)
+		HX_STACK_THIS(this)
+		HX_STACK_LINE(20)
+		this->super::create();
+	}
+return null();
+}
+
+
+Void MenuState_obj::destroy( ){
+{
+		HX_STACK_FRAME("MenuState","destroy",0xf9ac905e,"MenuState.destroy","MenuState.hx",29,0xdfbcb22c)
+		HX_STACK_THIS(this)
+		HX_STACK_LINE(29)
+		this->super::destroy();
+	}
+return null();
+}
+
+
+Void MenuState_obj::update( ){
+{
+		HX_STACK_FRAME("MenuState","update",0xf0719b25,"MenuState.update","MenuState.hx",37,0xdfbcb22c)
+		HX_STACK_THIS(this)
+		HX_STACK_LINE(37)
+		this->super::update();
+	}
+return null();
+}
+
+
+
+MenuState_obj::MenuState_obj()
+{
+}
+
+Dynamic MenuState_obj::__Field(const ::String &inName,bool inCallProp)
+{
+	switch(inName.length) {
+	case 6:
+		if (HX_FIELD_EQ(inName,"create") ) { return create_dyn(); }
+		if (HX_FIELD_EQ(inName,"update") ) { return update_dyn(); }
+		break;
+	case 7:
+		if (HX_FIELD_EQ(inName,"destroy") ) { return destroy_dyn(); }
+	}
+	return super::__Field(inName,inCallProp);
+}
+
+Dynamic MenuState_obj::__SetField(const ::String &inName,const Dynamic &inValue,bool inCallProp)
+{
+	return super::__SetField(inName,inValue,inCallProp);
+}
+
+void MenuState_obj::__GetFields(Array< ::String> &outFields)
+{
+	super::__GetFields(outFields);
+};
+
+static ::String sStaticFields[] = {
+	String(null()) };
+
+#if HXCPP_SCRIPTABLE
+static hx::StorageInfo *sMemberStorageInfo = 0;
+#endif
+
+static ::String sMemberFields[] = {
+	HX_CSTRING("create"),
+	HX_CSTRING("destroy"),
+	HX_CSTRING("update"),
+	String(null()) };
+
+static void sMarkStatics(HX_MARK_PARAMS) {
+	HX_MARK_MEMBER_NAME(MenuState_obj::__mClass,"__mClass");
+};
+
+#ifdef HXCPP_VISIT_ALLOCS
+static void sVisitStatics(HX_VISIT_PARAMS) {
+	HX_VISIT_MEMBER_NAME(MenuState_obj::__mClass,"__mClass");
+};
+
+#endif
+
+Class MenuState_obj::__mClass;
+
+void MenuState_obj::__register()
+{
+	hx::Static(__mClass) = hx::RegisterClass(HX_CSTRING("MenuState"), hx::TCanCast< MenuState_obj> ,sStaticFields,sMemberFields,
+	&__CreateEmpty, &__Create,
+	&super::__SGetClass(), 0, sMarkStatics
+#ifdef HXCPP_VISIT_ALLOCS
+    , sVisitStatics
+#endif
+#ifdef HXCPP_SCRIPTABLE
+    , sMemberStorageInfo
+#endif
+);
+}
+
+void MenuState_obj::__boot()
+{
+}
+
