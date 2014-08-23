87b026d39d5af8cb231c83a00b1cb870cd6e9bfe
Josh Schonstal <josh@schonstal.org>
2014-08-23 15:38:40 -0700

    Add rooms

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,12 +5,15 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
-import flixel.util.FlxMath;
 
 class PlayState extends FlxState
 {
+  var room:Room;
+
   override public function create():Void {
     super.create();
+    room = new Room("assets/tilemaps/quarters.tmx");
+    add(room.foregroundTiles);
   }
   
   override public function destroy():Void {
