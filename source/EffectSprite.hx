4a5571e0f7051c6f0b22656a4a68e65047df12fd
Josh Schonstal <josh@schonstal.org>
2014-08-23 17:10:05 -0700

    Basic palette swap

--- source/EffectSprite.hx
+++ source/EffectSprite.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flash.geom.Point;
+import flash.geom.Rectangle;
 import flash.filters.ColorMatrixFilter;
 
 class EffectSprite extends FlxSprite
@@ -19,6 +20,24 @@ class EffectSprite extends FlxSprite
 #else
     pixels.draw(FlxG.camera.canvas, new Matrix(1, 0, 0, 1, 0, 0));
 #end
+    var redArray:Array<Int> = new Array<Int>();
+    var greenArray:Array<Int> = new Array<Int>();
+    var blueArray:Array<Int> = new Array<Int>();
+
+    for(i in 0...255) {
+        redArray[i] = i << 16;
+        greenArray[i] = i << 8;
+        blueArray[i] = i;
+    }
+    redArray[0x7D] = 0x00330000;
+    greenArray[0x7D] = 0x0000ff00;
+    blueArray[0x7D] = 0x00000033;
+    redArray[0x55] = 0x00ff0000;
+    greenArray[0x55] = 0x00000000;
+    blueArray[0x55] = 0x000000ff;
+
+    pixels.paletteMap(pixels, new Rectangle(0,0,width,height), new Point(0,0), redArray, greenArray, blueArray);
+    
     resetFrameBitmapDatas();
     dirty = true;
     super.draw();
