80357afc38410d2f58b3595d9cac44d46161fd95
Josh Schonstal <josh@schonstal.org>
2014-08-25 18:04:07 -0700

    textpoop

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -13,30 +13,48 @@ class MenuState extends FlxState
 {
   var menuGraphic:FlxSprite;
   var startGameButton:FlxButton;
+  var indexButton:FlxButton;
   var effectSprite:EffectSprite;
+  var glitchSprite:GlitchSprite;
 
   override public function create():Void {
     menuGraphic = new FlxSprite();
     menuGraphic.loadGraphic("assets/images/titleScreen.png");
     add(menuGraphic);
 
-    startGameButton = new FlxButton(89,130, function():Void{
-      FlxG.switchState(new AlarmState());
-    });
-    startGameButton.loadGraphic("assets/images/startGame.png", true, 136, 10);
-    startGameButton.width = 150;
-    startGameButton.offset.x = -7;
-    startGameButton.height = 20;
-    startGameButton.offset.y = -5;
-    add(startGameButton);
     FlxG.debugger.drawDebug = true;
     super.create();
+
+    if(Reg.level == 5) {
+      indexButton = new FlxButton(89,130, function():Void{
+        glitchSprite.glitchOut();
+      });
+      indexButton.loadGraphic("assets/images/inbox.png", true, 92, 16);
+      indexButton.width = 150;
+      indexButton.offset.x = -7;
+      indexButton.height = 20;
+      indexButton.offset.y = -5;
+      add(indexButton);
+    } else {
+      startGameButton = new FlxButton(89,130, function():Void{
+        FlxG.switchState(new AlarmState());
+      });
+      startGameButton.loadGraphic("assets/images/startGame.png", true, 136, 10);
+      startGameButton.width = 150;
+      startGameButton.offset.x = -7;
+      startGameButton.height = 20;
+      startGameButton.offset.y = -5;
+      add(startGameButton);
+    }
 
     add(new Cursor());
 
     effectSprite = new EffectSprite();
     effectSprite.paletteRate = 0.2;
     add(effectSprite);
+
+    glitchSprite = new GlitchSprite();
+    add(glitchSprite);
 
     Reg.palette = 1;
     FlxG.sound.play("assets/sounds/spacestation.mp3", 0.75, true);
--- source/RestartState.hx
+++ source/RestartState.hx
@@ -17,7 +17,44 @@ class RestartState extends FlxState
     var bgSprite:FlxSprite = new FlxSprite();
     bgSprite.makeGraphic(FlxG.width, FlxG.height, 0xff091327);
 
-    panicText = new FlxText(0,0,FlxG.width,"FATAL ERROR: Critical fault!
+    var text:String;
+
+    if (Reg.level == 4) {
+"FATAL ERROR: Critical fault!
+Program data is corrupt.
+
+01 13 00ff 01   0000
+0bad0 00aa 01   0ddd
+01 10 00aa 0000 0
+
+Failed to sync... restarting.";
+    } else if (Reg.level == 5) {
+      text =
+"==========#=========
+==========#=========
+######====#==######=
+#######===#==#######
+#######===#==#######
+#######====#==######
+###========#======##
+===========#========
+===========##=======
+============#=======
+============##======
+=============#======
+=============##=====
+=============##=====
+=============##=====
+===========####=====";
+    } else if (Reg.level = 6) {
+      text =
+"HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP" +
+"HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP";
+    } else if (Reg.level = 7) {
+      text = "I don't want to be deleted...";
+    } else {
+      text =
+"FATAL ERROR: Critical fault!
 Program data is corrupt.
 
 [   342.545660004] 0ff0b000 000d005 0000bc000 0ff0b050
@@ -26,22 +63,32 @@ Program data is corrupt.
 
 00 43 5a 8b e9 ff 00 00 00 00 00 00 00 00 00
 
-Failed to sync... restarting.");
+Failed to sync... restarting.";
+    }
+
+    panicText = new FlxText(0,0,FlxG.width, text);
 
     panicText.setFormat("assets/fonts/437.ttf");
     panicText.size = 16;
     panicText.color = 0xff999999;
     add(panicText);
     
-    if(Reg.level <= 1) panicText.visible = false;
+    if(Reg.level <= 1 || Reg.level == 8) panicText.visible = false;
 
     new FlxTimer().start(1, function(t):Void {
       panicText.visible = false;
 
-      new FlxTimer().start(0.5, function(t):Void {
-        Reg.level++;
-        FlxG.switchState(new BadingState());
-      });
+      if(Reg.level < 8) {
+        new FlxTimer().start(0.5, function(t):Void {
+          Reg.level++;
+          FlxG.switchState(new BadingState());
+        });
+      } else {
+        //Credits
+        new FlxTimer().start(5, function(t):Void {
+          FlxG.switchState(new BadingState());
+        });
+      }
     });
   }
 }
